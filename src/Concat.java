07b1784bfca5187429f5cae970807345b6ef4165
eur\om <om@softwareag.com>
2022-06-30 20:17:22 +0530

    first program

---
+++ src/Concat.java
@@ -0,0 +1,9 @@
+
+public class Concat {
+
+	
+	public static void main (String args[]) {
+		System.out.println(100+100+"Raja");
+		System.out.println("Shailendra" +100+100);
+	}
+}
---
+++ src/StringReverse.java
@@ -0,0 +1,16 @@
+
+public class StringReverse {
+	
+	//Method1
+	public static void main(String args[]) {
+		String s1="Shailendra";
+		String s2 = new StringBuffer(s1).reverse().toString();
+		System.out.println(s2);
+		
+		
+		
+		//Method2
+		
+	}
+
+}
