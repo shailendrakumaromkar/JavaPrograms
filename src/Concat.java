ec2d1bd9b2a6fa77befcc8d143db5c2172571b2e
shailendrakumaromkar <shailendrakumaromkar@gmail.com>
2022-07-12 20:42:48 +0530

    added programs

--- src/Concat.java
+++ src/Concat.java
@@ -4,6 +4,6 @@ public class Concat {
 	
 	public static void main (String args[]) {
 		System.out.println(100+100+"Raja");
-		System.out.println("Shailendra" +100+100);
+		System.out.println("Shailendra Kumar" +100+100);
 	}
 }
